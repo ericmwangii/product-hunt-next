ea7b4057429bd4a89da94eff62d6e54b87c33d84
ericmwangii <ericmwangii101@gmail.com>
2022-12-19 10:46:32 +0300

    f

--- components/Item.tsx
+++ components/Item.tsx
@@ -13,26 +13,24 @@ export const Item: React.FC<itemInfo> = ({
 }) => {
   return (
     <div className="col">
-      <Link href={itemUrl}>
-        <div className="card h-100">
-          <Image
-            src={imageUrl}
-            className="card-img-top"
-            alt="product-photo"
-            width={340}
-            height={300}
-          />
-          <div className="card-body">
-            <h5 className="card-title">
-              <strong>{price}</strong>
-            </h5>
-            <p className="card-text">{name}</p>
-            <p className="card-text">
-              <strong>From {source}</strong>
-            </p>
-          </div>
+      <div className="card h-100">
+        <Image
+          src={imageUrl}
+          className="card-img-top"
+          alt="product-photo"
+          width={340}
+          height={300}
+        />
+        <div className="card-body">
+          <h5 className="card-title">
+            <strong>{price}</strong>
+          </h5>
+          <p className="card-text">{name}</p>
+          <p className="card-text">
+            <strong>From {source}</strong>
+          </p>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
