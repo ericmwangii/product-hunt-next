b405db2d0691b125d0a8d430ef846041ecd448ce
ericmwangii <ericmwangii101@gmail.com>
2022-12-19 10:33:07 +0300

    f

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 import { itemInfo } from "../interface/itemInfo";
 
 export const Item: React.FC<itemInfo> = ({
@@ -12,24 +13,26 @@ export const Item: React.FC<itemInfo> = ({
 }) => {
   return (
     <div className="col">
-      <div className="card h-100">
-        <Image
-          src={imageUrl}
-          className="card-img-top"
-          alt="product-photo"
-          width={340}
-          height={300}
-        />
-        <div className="card-body">
-          <h5 className="card-title">
-            <strong>{price}</strong>
-          </h5>
-          <p className="card-text">{name}</p>
-          <p className="card-text">
-            <strong>From {source}</strong>
-          </p>
+      <Link href={itemUrl}>
+        <div className="card h-100">
+          <Image
+            src={imageUrl}
+            className="card-img-top"
+            alt="product-photo"
+            width={340}
+            height={300}
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              <strong>{price}</strong>
+            </h5>
+            <p className="card-text">{name}</p>
+            <p className="card-text">
+              <strong>From {source}</strong>
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
